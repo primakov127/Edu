a7b02f0eb9d46a46fb9dc1825dbfb9d98f25d9b0
primakov127 <primakov127@gmail.com>
2021-03-01 19:39:57 +0300

    Implemented Singleton pattern

---
+++ Design Patterns/Design Patterns/Patterns/Singleton/DataBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design_Patterns.Patterns.Singleton
+{
+    public class DataBase : IDisposable
+    {
+        private static Connection _connection;
+
+        public void Open()
+        {
+            if (_connection == null)
+                _connection = Connection.GetInstance();
+        }
+
+        public string Query(string queryString)
+        {
+            return _connection.Query(queryString);
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+            _connection = null;
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+    }
+}
--- Design Patterns/Design Patterns/Program.cs
+++ Design Patterns/Design Patterns/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Design_Patterns.Patterns.Singleton;
+using System;
 
 namespace Design_Patterns
 {
@@ -6,7 +7,12 @@ namespace Design_Patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new DataBase())
+            {
+                db.Open();
+                string queryResult = db.Query("SELECT * FROM USERS");
+                Console.WriteLine(queryResult);
+            }
         }
     }
 }
