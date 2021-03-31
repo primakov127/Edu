300b98fd58821512a0cb72b746a983a6a48a94cc
Maxim Primakov <42981680+primakov127@users.noreply.github.com>
2021-03-31 22:43:35 +0300

    Added changes requested by Sergey Gontsarevich

--- React/Progress bar/src/components/App.js
+++ React/Progress bar/src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ProgressBar from "./ProgressBar";
 
 const App = () => {
-  return <ProgressBar value={10}></ProgressBar>;
+  return <ProgressBar value={10} />;
 };
 
 export default App;
