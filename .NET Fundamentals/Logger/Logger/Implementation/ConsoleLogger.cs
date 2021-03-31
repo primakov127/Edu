83c1c32829eb8131b3115b515241ce0a8ec50d9f
primakov127 <primakov127@gmail.com>
2021-03-31 15:50:37 +0300

    Implemented CS Logger

---
+++ .NET Fundamentals/Logger/Logger/Implementation/ConsoleLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Logger.Implementation
+{
+    public class ConsoleLogger : ILogger
+    {
+        private const string ERROR_LABEL = "[ERROR]";
+        private const string WARNING_LABEL = "[WARNING]";
+        private const string INFO_LABEL = "[INFO]";
+
+        public void Error(string message)
+        {
+            Console.WriteLine($"{ERROR_LABEL}: {message}");
+        }
+
+        public void Error(Exception ex)
+        {
+            string message = ex.ToString();
+            Console.WriteLine($"{ERROR_LABEL}: {message}");
+        }
+
+        public void Info(string message)
+        {
+            Console.WriteLine($"{INFO_LABEL}: {message}");
+        }
+
+        public void Warning(string message)
+        {
+            Console.WriteLine($"{WARNING_LABEL}: {message}");
+        }
+    }
+}
---
+++ .NET Fundamentals/Logger/Logger/Implementation/FileLogger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace Logger.Implementation
+{
+    public class FileLogger : ILogger
+    {
+        private const string ERROR_LABEL = "[Error]";
+        private const string WARNING_LABEL = "[Warning]";
+        private const string INFO_LABEL = "[Info]";
+
+        private readonly string _filePath;
+
+        public string FilePath => _filePath;
+
+        public FileLogger(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Error(string message)
+        {
+            string formattedMsg = GetFormattedLine(ERROR_LABEL, message, DateTime.Now);
+            WriteFileLine(formattedMsg);
+        }
+
+        public void Error(Exception ex)
+        {
+            string formattedMsg = GetFormattedLine(ERROR_LABEL, ex.ToString(), DateTime.Now);
+            WriteFileLine(formattedMsg);
+        }
+
+        public void Info(string message)
+        {
+            string formattedMsg = GetFormattedLine(INFO_LABEL, message, DateTime.Now);
+            WriteFileLine(formattedMsg);
+        }
+
+        public void Warning(string message)
+        {
+            string formattedMsg = GetFormattedLine(WARNING_LABEL, message, DateTime.Now);
+            WriteFileLine(formattedMsg);
+        }
+
+        private string GetFormattedLine(string label, string message, DateTime time)
+        {
+            string formattedLabel = String.Format("{0, -10}", label);
+            string formattedTime = time.ToString("yyyy/MM/dd HH:mm:ss:ff");
+
+            string fullFormattedLine = $"{formattedLabel} | {formattedTime} | {message}";
+
+            return fullFormattedLine;
+        }
+
+        private void WriteFileLine(string text)
+        {
+            string newLineText = $"{text}\n";
+            using (var fStream = new FileStream(_filePath, FileMode.Append, FileAccess.Write))
+            {
+                byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(newLineText);
+                fStream.Write(byteArray);
+            }
+        }
+    }
+}
