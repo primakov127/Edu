378c3bc71b4b0bb450ea54e6ebdadf84cc482af9
primakov127 <primakov127@gmail.com>
2021-04-01 14:24:06 +0300

    Merge pull request #10 from primakov/webPage-wp

---
+++ HTML_CSS/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        publicPath: "./",
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css"})
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(css|scss)$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.html$/,
+                loader: "html-loader",
+                options: {
+                    esModule: false
+                }
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
+            }
+        ]
+    },
+    // resolve: {
+    //     roots: [path.resolve(__dirname, "imgs")]
+    // }
+}
