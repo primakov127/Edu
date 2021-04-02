fe565c8e3bf176804626c456f36fd58accfddb60
primakov127 <primakov127@gmail.com>
2021-04-02 15:36:52 +0300

    Implemented CS Csv Enumerable

---
+++ .NET Fundamentals/CsvEnumerable/CsvEnumerable/CsvEnumerable.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CsvEnumerable
+{
+    public class CsvEnumerable<T> : IEnumerable<T>
+    {
+        private readonly string _filePath;
+
+        public CsvEnumerable(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            using (var reader = new StreamReader(_filePath))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    foreach (var record in csv.GetRecords<T>())
+                    {
+                        yield return record;
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
