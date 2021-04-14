4c80ef990960440745d829eaf018fffecc313ef2
primakov127 <primakov127@gmail.com>
2021-04-14 18:10:06 +0300

    Created CoordsModelBinder class

---
+++ .NET ASP.NET Core/ModelBinders/ModelBinders/ModelBinders/CoordsModelBinder.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ModelBinders.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinders.ModelBinders
+{
+    public class CoordsModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue("coord");
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            var coordValue = valueProviderResult.FirstValue;
+
+            if (String.IsNullOrEmpty(coordValue))
+            {
+                return Task.CompletedTask;
+            }
+
+            var coordsArray = coordValue.Split(',').Select(c => int.Parse(c)).ToArray();
+            bindingContext.Result = ModelBindingResult.Success(coordsArray);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ .NET ASP.NET Core/ModelBinders/ModelBinders/Models/Point.cs
@@ -0,0 +1,9 @@
+﻿namespace ModelBinders.Models
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+    }
+}
