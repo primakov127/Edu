9ea3b24302f5195af24100a0e0ea81c4bea2781f
primakov127 <primakov127@gmail.com>
2021-04-14 18:40:53 +0300

    Implemented PersonIdModelBinder class

---
+++ .NET ASP.NET Core/ModelBinders/ModelBinders/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelBinders.ModelBinders;
+using ModelBinders.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelBinders.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet("{personId}")]
+        public ActionResult Get([ModelBinder(typeof(PersonIdModelBinder))]Guid personId)
+        {
+            var person = new Person { Id = personId, Name = "Max" };
+            return Ok(person);
+        }
+    }
+}
---
+++ .NET ASP.NET Core/ModelBinders/ModelBinders/ModelBinders/PersonIdModelBinder.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelBinders.ModelBinders
+{
+    public class PersonIdModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue("personId");
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            var userIdValue = valueProviderResult.FirstValue;
+
+            if (String.IsNullOrEmpty(userIdValue))
+            {
+                return Task.CompletedTask;
+            }
+
+            var bytes = Convert.FromBase64String(userIdValue);
+            var decodedUserId = Encoding.UTF8.GetString(bytes);
+            var result = new Guid(decodedUserId);
+            bindingContext.Result = ModelBindingResult.Success(result);
+            return Task.CompletedTask;
+        }
+    }
+}
